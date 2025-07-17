93131a34c2ac7c2f0b7eb2ca2c2e33f4296ea06f
Meriiiiieee <meri.hakobyan21@edu.ysu.am>
2025-07-17 16:50:15 +0400

    Create mul.asm

---
+++ Mul.asm
@@ -0,0 +1,51 @@
+ extern printf
+ extern scanf
+
+ section .data
+         first_msg db "Enter first number: ", 10, 0
+         frmt db "%d", 0
+	       second_msg db "Enter second number: ", 10, 0
+         frmt1 db "%d", 0
+         product_msg db "Product %d",10, 0
+
+ section .bss
+         num1 resd 1
+ 	       num2 resd 1
+
+ section .text
+      
+ 	        global main
+  
+ main:
+         push rbp
+  	     mov rbp, rsp
+
+         mov rdi, first_msg
+	       xor rax, rax 
+         call printf
+
+	       mov rdi, frmt
+	       lea rsi, [num1]
+	       xor rax, rax
+	       call scanf
+
+         mov rdi, second_msg
+	       xor rax, rax 
+	       call printf
+
+	       mov rdi, frmt1
+	       lea rsi, [num2]
+	       xor rax, rax
+	       call scanf
+
+	       mov rax, [num1]
+	       mov r9d, [num2]
+	       mul  r9d
+
+	       mov rdi, product_msg
+	       mov rsi, rax
+	       call printf
+
+	       mov rsp, rbp
+	       pop rbp
+	       ret
